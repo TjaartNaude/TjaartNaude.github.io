4756c3d8d2ba16f218320dce82c7d681fd95590a
TjaartNaude <77753514+TjaartNaude@users.noreply.github.com>
2023-10-10 00:00:56 -0500

    removed inactive router link

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,89 +1,81 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/DashboardPage.vue') }
-    ]
+      { path: "", component: () => import("pages/DashboardPage.vue") },
+    ],
   },
   {
-    path: '/requests',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/RequestsPage.vue') }
-    ]
+    path: "/requests",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/RequestsPage.vue") }],
   },
   {
-    path: '/newrequest',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/newrequest",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/NewRequestPage.vue') }
-    ]
+      { path: "", component: () => import("pages/NewRequestPage.vue") },
+    ],
   },
   {
-    path: '/login',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/SignIn.vue') }
-    ]
+    path: "/login",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/SignIn.vue") }],
   },
   {
-    path: '/dashboard',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/dashboard",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/DashboardPage.vue') }
-    ]
+      { path: "", component: () => import("pages/DashboardPage.vue") },
+    ],
   },
   {
-    path: '/reports',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/ReportsPage.vue') }
-    ]
+    path: "/reports",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/ReportsPage.vue") }],
   },
   {
-    path: '/knowledge',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/knowledge",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/KnowledgePage.vue') }
-    ]
+      { path: "", component: () => import("pages/KnowledgePage.vue") },
+    ],
   },
   {
-    path: '/users',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/users",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/ManageUserPage.vue') } 
-    ]
+      { path: "", component: () => import("pages/ManageUserPage.vue") },
+    ],
   },
   {
-    path: '/createuser',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/createuser",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/CreateUserPage.vue') }
-    ]
+      { path: "", component: () => import("pages/CreateUserPage.vue") },
+    ],
   },
   {
-    path: '/settings',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/SettingsPage.vue') }
-    ]
+    path: "/settings",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/SettingsPage.vue") }],
   },
   {
-    path: '/priorities',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/priorities",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/PrioritiesPage.vue') }
-    ]
+      { path: "", component: () => import("pages/PrioritiesPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
+  /**
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+  }*/
+];
 
-export default routes
+export default routes;
